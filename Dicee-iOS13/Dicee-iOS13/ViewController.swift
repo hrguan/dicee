9621787a967cd050880fdbd3febcea38537a3330
hrguan <80980556+hrguan@users.noreply.github.com>
2023-06-20 16:36:51 -0700

    Add files via upload
    init

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+    
+    @IBAction func button(_ sender: UIButton) {
+        
+        let diceArray = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+         
+        dice1.image = diceArray[Int.random(in: 0...5)]
+        
+        dice2.image = diceArray[Int.random(in: 0...5)]
+    }
+    
+}
+
